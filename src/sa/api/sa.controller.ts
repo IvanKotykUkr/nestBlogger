504400dbe1de981bed1acf4f3b305b38df932100
IvanKotykUkr <iwankot3@gmail.com>
2023-04-13 21:38:28 +0100

    add sa

--- src/sa/api/sa.controller.ts
+++ src/sa/api/sa.controller.ts
@@ -2,22 +2,46 @@ import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import {
   Body,
   Controller,
+  Get,
   HttpCode,
   Ip,
   Post,
+  Query,
   UseGuards,
 } from '@nestjs/common';
 import { AntiDdosGuard } from '../../auth/application/adapters/guards/anti.ddos.guard';
 import { CreateUserGuard } from '../../guards/create.user.guard';
-import { BodyForCreateUser } from '../../users/users.types';
+import {
+  BodyForCreateUser,
+  QueryForGetUsers,
+  UsersWithPaginationResponseType,
+} from '../../users/users.types';
 import { CreateUserCommand } from '../../users/application/use.case/create.user.use.case';
 import { ConfirmationType, EmailBodyType } from '../../auth/auth.types';
 import { ConfirmCodeUserCommand } from '../../auth/application/use.case/confirm.code.user.use.case';
 import { ResendConfirmCodeUserCommand } from '../../auth/application/use.case/resend.confirm.code.user.use.case';
+import { QueryUsersRepositories } from '../../users/infrastructure/query.users.repositories';
 
 @Controller('/sa')
 export class SAController {
-  constructor(protected commandBus: CommandBus, protected queryBus: QueryBus) {}
+  constructor(
+    protected commandBus: CommandBus,
+    protected queryBus: QueryBus,
+    protected queryUsersRepositories: QueryUsersRepositories,
+  ) {}
+
+  @Get('/users')
+  async getUsers(
+    @Query() query: QueryForGetUsers,
+  ): Promise<UsersWithPaginationResponseType> {
+    const pageNumber = query.PageNumber || 1;
+    const pageSize = query.PageSize || 10;
+    const user = await this.queryUsersRepositories.findAllUsersWithPagination(
+      pageNumber,
+      pageSize,
+    );
+    return user;
+  }
 
   @UseGuards(AntiDdosGuard, CreateUserGuard)
   @HttpCode(201)
